1c15819f4e86569efaa0dcee32ec33b7fce580c1
Ravi Vooda <rvooda@freewheel.tv>
2016-11-12 06:15:24 +0800

    Creating Android project

---
+++ Android/ContactCard/app/src/test/java/utils/com/contactcard/ExampleUnitTest.java
@@ -0,0 +1,24 @@
+/*
+ * Created by Ravi Vooda on 11/12/16 6:12 AM
+ * Copyright (c) 2016. All rights reserved.
+ *
+ * Last modified 11/12/16 6:02 AM
+ */
+
+package utils.com.contactcard;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
