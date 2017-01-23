35d0826da9ab88ff62593941581450d407215ac1
Ravi Vooda <r.vooda@gmail.com>
2017-01-23 18:03:29 -0500

    Adding and deleting the new card request

---
+++ iOS/Contact Card/Contact Card/Models/Card/CCCard.swift
@@ -0,0 +1,13 @@
+//
+//  CCCard.swift
+//  Contact Card
+//
+//  Created by Ravi Vooda on 1/23/17.
+//  Copyright © 2017 Utils. All rights reserved.
+//
+
+import UIKit
+
+class CCCard: NSObject {
+
+}
--- iOS/Contact Card/Contact Card/View Controllers/My Cards/CCMyCardsViewController.swift
+++ iOS/Contact Card/Contact Card/View Controllers/My Cards/CCMyCardsViewController.swift
@@ -7,24 +7,47 @@
 //
 
 import UIKit
+import Contacts
+import ContactsUI
 
-class CCMyCardsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-	@IBOutlet weak var tableView: UITableView!
-
-	@IBAction func addNewCard(_ sender: Any) {
-		
-	}
-	
-	//MARK: - UITableViewDataSource -
-	
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 1
-	}
-	
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let cell = tableView.dequeueReusableCell(withIdentifier: "cardTableViewCellIdentifier", for: indexPath)
-		return cell
-	}
-	
-	//MARK: - UITableViewDelegate -
+class CCMyCardsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CNContactViewControllerDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func addNewCard(_ sender: Any) {
+        let newCardViewController = CNContactViewController(forNewContact: nil)
+        newCardViewController.contactStore = CNContactStore()
+        newCardViewController.delegate = self
+        self.present(UINavigationController(rootViewController: newCardViewController), animated: true, completion: nil)
+    }
+    
+    //MARK: - UITableViewDataSource -
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cardTableViewCellIdentifier", for: indexPath)
+        return cell
+    }
+    
+    //MARK: - UITableViewDelegate -
+    
+    //MARK: - CNContactViewControllerDelegate -
+    func contactViewController(_ viewController: CNContactViewController, didCompleteWith contact: CNContact?) {
+        print("Completed adding card \(contact)")
+        viewController.dismiss(animated: true) {
+            if let _contact = contact?.mutableCopy() as? CNMutableContact{
+                // Deleting the contact first
+                let deleteRequest = CNSaveRequest()
+                deleteRequest.delete(_contact)
+                
+                do {
+                    try CNContactStore().execute(deleteRequest)
+                } catch let error as NSError {
+                    print(error)
+                }
+            }
+        }
+    }
 }
