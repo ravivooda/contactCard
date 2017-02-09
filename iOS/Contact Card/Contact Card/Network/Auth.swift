d60ee8a1dd200f14eb29edbe842aceeed0b6bead
Ravi Vooda <rvooda@freewheel.tv>
2017-02-08 22:04:57 -0500

    LoginViewController

--- iOS/Contact Card/Contact Card/Network/Auth.swift
+++ iOS/Contact Card/Contact Card/Network/Auth.swift
@@ -10,7 +10,8 @@ import Foundation
 import UIKit
 
 extension Data {
-    static func login(_ username:String, password:String, callingViewController:UIViewController) {
-        
+	static func login(_ username:String, password:String, callingViewController:UIViewController, success:Success?, fail:Fail?) {
+		//http://api/login?email=test@test.com&password=test
+		api(.get, api: "login", parameters: ["email":"test@test.com", "password":"test"], viewController: callingViewController, success: success, fail: fail)
     }
 }
---
+++ iOS/Contact Card/Contact Card/View Controllers/Auth/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  Contact Card
+//
+//  Created by Ravi Vooda on 2/8/17.
+//  Copyright © 2017 Utils. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+	@IBOutlet weak var loginInputContainer: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+	
+	@IBAction func loginClicked(_ sender: UIButton) {
+		
+	}
+	
+	
+	@IBAction func signupClicked(_ sender: UIButton) {
+		
+	}
+}
