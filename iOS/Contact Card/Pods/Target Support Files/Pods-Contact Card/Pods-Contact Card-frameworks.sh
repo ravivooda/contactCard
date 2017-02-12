b48b0575a027c2e007e0979c2812c95cdf915577
Ravi Vooda <r.vooda@gmail.com>
2017-02-12 11:57:24 -0500

    Adding pods IQKeyboardManagerSwift and SSKeychain

--- iOS/Contact Card/Pods/Target Support Files/Pods-Contact Card/Pods-Contact Card-frameworks.sh
+++ iOS/Contact Card/Pods/Target Support Files/Pods-Contact Card/Pods-Contact Card-frameworks.sh
@@ -85,9 +85,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Material/Material.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SSKeychain/SSKeychain.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Material/Material.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SSKeychain/SSKeychain.framework"
 fi
